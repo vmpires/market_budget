44148c2a5fc1cb5894df51f1545b6dabe04d09e8
Victor Pires <vctprs@gmail.com>
2023-04-07 17:57:29 -0300

    .

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module MarketBudget
     config.load_defaults 5.2
     
     config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.time_zone = "Brasilia"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
